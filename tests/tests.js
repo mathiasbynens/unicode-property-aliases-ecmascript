99b53c52f18b6fc19f5f3d3340b9a6deda5b1811
Huáng Jùnliàng <jlhwung@gmail.com>
2025-09-15 03:31:23 -0400

    Update to Unicode v17.0.0 (#3)

--- tests/tests.js
+++ tests/tests.js
@@ -1,28 +1,28 @@
-const test = require('ava');
+const test = require('node:test');
 const propertyAliases = require('../index.js');
 
 test('unicode-property-aliases-ecmascript', t => {
-	t.is(
+	t.assert.ok(
 		propertyAliases.get('gc'),
 		'General_Category'
 	);
-	t.is(
+	t.assert.ok(
 		propertyAliases.get('sc'),
 		'Script'
 	);
-	t.is(
+	t.assert.ok(
 		propertyAliases.get('scx'),
 		'Script_Extensions'
 	);
-	t.is(
+	t.assert.ok(
 		propertyAliases.get('WSpace'),
 		'White_Space'
 	);
-	t.is(
+	t.assert.ok(
 		propertyAliases.get('space'),
 		'White_Space'
 	);
-	t.is(
+	t.assert.ok(
 		propertyAliases.get('EBase'),
 		'Emoji_Modifier_Base'
 	);
